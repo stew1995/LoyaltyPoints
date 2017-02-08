45a3595255566f122fbb0b41bac657aa1d3b51fe
stew1995 <up703451@myport.ac.uk>
2017-02-08 19:07:27 +0000

    Implented points need to link with users points Changed how the transcation page looks
    * dots removed - think about using the library circlepageindicator for the dots
    * Maps Page
    * The list at the bottom needs looking at
    *general redesign for this page is needed
    REGISTRATION PAGE
    symbols tectwatcher which needs checking
    
    *Validators implemented - email doesnt work
    
    ^^ NOT DONE

--- app/src/main/java/com/stewart/loyaltypoints/TransactionsActivity.java
+++ app/src/main/java/com/stewart/loyaltypoints/TransactionsActivity.java
@@ -1,5 +1,6 @@
 package com.stewart.loyaltypoints;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -32,6 +33,9 @@ public class TransactionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_transactions );
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
 
         //Firebase
         mRef = FirebaseDatabase.getInstance().getReference();
